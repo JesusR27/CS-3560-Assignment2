ba7431c45c36a53c1d84effbae0ae8022131cbcd
JesusR27 <113713053+JesusR27@users.noreply.github.com>
2023-03-07 13:37:30 -0800

    Add files via upload

---
+++ StudentTest.java
@@ -0,0 +1,21 @@
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		//initiating two Student objects
+		Student student1 = new Student("John", "CS", 3.5);
+		Student student2 = new Student();
+		
+		//updating details of student2
+		student2.setName("Mary Ann");
+		student2.setMajor("CE");
+		student2.setGpa(3.3);
+		
+		//printing the states of the two objects
+		System.out.println(student1.toString());
+		System.out.println(student2.toString());
+
+	}
+
+}
